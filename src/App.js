9915da9f36d9134ec0c27a7d7378de84c7cf1501
Chad Burkins <chad.burkins@gmail.com>
2019-12-31 10:38:52 -0500

    Added light gray Navbar at top, added 2nd Nav item pushed to the right side

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 export default function App() {
   return (
     <Router>
       <div>
-        <Nav>
-          <Nav.Item>
-            <Nav.Link href="/">Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/about">About</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/users">Users</Nav.Link>
-          </Nav.Item>
-        </Nav>
+        <Navbar bg="light">
+          {/* 1st Nav item, mr-auto pushes the 2nd Nav item to the right */}
+          <Nav className="mr-auto">
+            <Nav.Item>
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/about">About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/users">Users</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          {/* 2nd Nav item, gets pushed to right because of mr-auto on first item */}
+          <Nav>Status: Connected</Nav>
+        </Navbar>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
